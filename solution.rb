c19ec64c58e2b79fd2c300161d87d08204d763c4
Jeff Whitmire <jeff.whitmire@phishme.com>
2016-02-05 10:09:00 -0500

    DO THE MATHS

--- solution.rb
+++ solution.rb
@@ -1,1 +1,33 @@
 # Add your solution here
+
+{
+  zero: 0,
+  one: 1,
+  two: 2,
+  three: 3,
+  four: 4,
+  five: 5,
+  six: 6,
+  seven: 7,
+  eight: 8,
+  nine: 9
+}.each do |word, digit|
+  define_method(word) do |operation=nil|
+    if operation
+      eval("#{digit} #{operation}")
+    else
+      digit
+    end
+  end
+end
+
+{
+  plus: '+',
+  minus: '-',
+  times: '*',
+  divided_by: '/'
+}.each do |name, operator|
+  define_method(name) do |operand|
+    " #{operator} #{operand}"
+  end
+end
